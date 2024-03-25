c694c3e26275a05316e65d0aa7b0c3577f0ccf65
tinosauro <m.pelucchi@student.rug.nl>
2024-03-25 14:33:07 +0100

    new nam code + generate only 1 neighbour

--- main.py
+++ main.py
@@ -32,31 +32,44 @@ def generate_initial_state(G, k):
     nodes = list(G.nodes)
     random.shuffle(nodes)  # Shuffle nodes to randomly pick initial district centers
 
-    state = State(districts=[District(nodes[i]) for i in range(k)], k=k)
+    state = State(G=G, districts=[District(nodes[i]) for i in range(k)], k=k)
 
     # Repeat until all nodes have been assigned to a district
     while any(node not in (n for district in state.districts for n in district.nodes) for node in nodes):
         extend_cluster(G, state)
 
     update_all_boundaries(G, state)
+    for district in state.districts:
+        is_connected(district)
     return state
 
 
-def is_connected(district):
-    #TODO: check if district is not cut
-    return True
+def is_connected(district: District):
+    """
+    Check if a subgraph of a district is connected - all nodes are reachable.
+    """
+    if district.spanning_tree is None:
+        G_dis = nx.Graph()
+        G_dis.add_nodes_from(district.nodes)
+        for node in district.nodes:
+            for neighbor in G.neighbors(node):
+                if neighbor in district.nodes:
+                    G_dis.add_edge(node, neighbor)
+        district.update_spanning_tree(G_dis)
+    return nx.is_connected(district.spanning_tree)
 
 
-def generate_new_states(G, current_state):
-    new_states = []
-
+def generate_random_neighbour(G, current_state):
+    # Shuffle the list of districts to get random neighbouring state
+    shuffled_districts = current_state.districts.copy()
+    random.shuffle(shuffled_districts)
     for origin_district_index, origin_district in enumerate(current_state.districts):
 
         for node in origin_district.boundary:
 
             for neighbor in G.neighbors(node):
 
-                for destination_district_index, destination_district in enumerate(current_state.districts):
+                for destination_district_index, destination_district in enumerate(shuffled_districts):
 
                     if destination_district_index != origin_district_index and neighbor in destination_district.nodes:
 
@@ -71,31 +84,30 @@ def generate_new_states(G, current_state):
                         # Add the new state to the list of new states if it maintains connectivity
                         if is_connected(new_state.districts[origin_district_index]) and is_connected(
                                 new_state.districts[destination_district_index]):
-                            new_states.append(new_state)
-    return new_states
+                            return new_state
 
 
-def evaluate(state):
-    return 1
 
 
-def oba_search(G, initial_solution, thresh=0.5, iterations=100):
+
+#iterations seem to go from 20 to 80k
+#threshold seems to be in range 10^-7 to 10^-2 depending on target objective (compactness/population etc)
+def oba_search(G, initial_solution, thresh=0.1, iterations=50):
     current_solution = initial_solution
-    current_value = evaluate(initial_solution)
+    current_cost = initial_solution.fitness()
     best_solution = current_solution
-    best_value = current_value
+    best_cost = current_cost
     # careful with pass by value/reference, maybe need to make copies
     for iter in range(iterations):
-        neighbours = generate_new_states(G, current_solution)
-        new_solution = random.choice(neighbours)
-        new_value = evaluate(new_solution)
-        delta = new_value - current_value
+        new_solution = generate_random_neighbour(G,current_solution)
+        new_cost = new_solution.fitness()
+        delta = new_cost - current_cost
         if delta < thresh:
             current_solution = new_solution
 
-            if new_value < best_value:
+            if new_cost < best_cost:
                 best_solution = new_solution
-                best_value = new_value
+                best_cost = new_cost
             if delta < 0:
                 thresh = 1 - (iter / iterations)
         else:
@@ -105,12 +117,11 @@ def oba_search(G, initial_solution, thresh=0.5, iterations=100):
 
 
 if __name__ == '__main__':
-    G, total_population, k = load_data.load_data('data/RI')
+    G, total_population, k = load_data.load_data('data/RI') #state initials
     if not nx.is_connected(G):
         # If the graph is not connected, consider the largest connected component.
         G = G.subgraph(max(nx.connected_components(G), key=len))
 
     initial_state = generate_initial_state(G, k)
-
-    new_states = generate_new_states(G, initial_state)
-    print("1")
+    solution = oba_search(G, initial_state)
+    print(solution)
--- state.py
+++ state.py
@@ -1,5 +1,9 @@
+import networkx as nx
+
+
 class State:
-    def __init__(self, districts=None, k=1):
+    def __init__(self, G, districts=None, k=1):
+        self.G = G
         self.districts = districts
         self.k = k
 
@@ -7,15 +11,53 @@ class State:
         self.districts.append(district)
         self.k = len(self.districts)
 
+    def fitness(self):
+        imbalance = self.population_imbalance() + 1
+        distances = self.node_distances() + 1
+        fitness = 0.5 * (1 / imbalance) + 0.5 * (1 / distances)
+        return fitness
+
+    def population_imbalance(self):
+        populations = []
+        for district in self.districts:
+            district_population = sum([self.G.nodes[node]['pop'] for node in district.nodes])
+            populations.append(district_population)
+
+        return max(populations) - min(populations)
+
+    def node_distances(self):
+        """
+        Compute the average of the average distances between all nodes in each district
+        :return:
+        """
+        avg_distances = []
+        for district in self.districts:
+
+            path_lengths = dict(nx.all_pairs_shortest_path_length(district.spanning_tree))
+
+            # Flatten the list of lengths to a single list containing all lengths
+            lengths = [length for target_lengths in path_lengths.values() for length in target_lengths]
+
+            # Compute the average distance
+            avg_distance = sum(lengths) / len(lengths)
+            avg_distances.append(avg_distance)
+
+        return sum(avg_distances) / len(avg_distances)
+
 
 class District:
     def __init__(self, first_node):
         self.nodes = [first_node]
         self.boundary = []
         self.cocycle = []
+        self.spanning_tree = None
 
     def add_node(self, node):
         self.nodes.append(node)
 
     def delete_node(self, node):
         self.nodes.remove(node)
+        self.boundary.remove(node)
+
+    def update_spanning_tree(self, G):
+        self.spanning_tree = G
